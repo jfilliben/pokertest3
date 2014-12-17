e482f4e730f610f614650fc006b9bf328af32e84
Jeremy Filliben <jeremyfilliben@Jeremys-Mac-mini.local>
2014-12-17 08:33:55 -0500

    Initial Commit

---
+++ pokertest3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokertest3
+//
+//  Created by Jeremy Filliben on 12/17/14.
+//  Copyright (c) 2014 Jeremy Filliben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pokertest3Tests/pokertest3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  pokertest3Tests.swift
+//  pokertest3Tests
+//
+//  Created by Jeremy Filliben on 12/17/14.
+//  Copyright (c) 2014 Jeremy Filliben. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class pokertest3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
